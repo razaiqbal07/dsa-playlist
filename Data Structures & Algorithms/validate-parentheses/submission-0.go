0ae14c1becc2f275f9e09123be82d5c74618dc19
Raza Iqbal <158839085+razaiqbal07@users.noreply.github.com>
2026-07-06 22:44:06 +0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.go
@@ -0,0 +1,32 @@
+func isValid(s string) bool {
+    check := []string{} 
+    
+    validMap := map[string]string{
+        "(": ")",
+        "[": "]",
+        "{": "}",
+    }
+    
+    for _, ch := range s {
+        strCh := string(ch)
+        
+        if strCh == "(" || strCh == "{" || strCh == "[" {
+            check = append(check, strCh)
+        } else {
+			if len(check) == 0 {
+                return false
+            }
+            
+            lastIndex := len(check) - 1
+            lastItem := check[lastIndex] 
+            
+            if validMap[lastItem] != strCh {
+                return false
+            }
+            
+            check = check[:lastIndex]
+        }
+    }
+    
+    return len(check) == 0
+}
