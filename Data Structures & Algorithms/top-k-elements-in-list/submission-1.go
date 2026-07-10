25a0c5267e7cbf43efd2cedb049506ec5b26b2ef
Raza Iqbal <158839085+razaiqbal07@users.noreply.github.com>
2026-07-10 21:54:11 +0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.go
@@ -0,0 +1,16 @@
+func topKFrequent(nums []int, k int) []int {
+    result := []int{}
+    frequencyBucket := make([][]int,len(nums) + 1)
+    frequencyMap := map[int]int{}
+    for i:=0;i<len(nums);i++ {
+        frequencyMap[nums[i]] = frequencyMap[nums[i]] + 1
+    }
+    for key,value := range frequencyMap {
+        frequencyBucket[value] = append(frequencyBucket[value],key)
+    }
+    for i := len(frequencyBucket) - 1;i>=0;i-- {
+        result =append(result,frequencyBucket[i]...)
+        if len(result) >= k {return result[:k]}
+    }
+    return result
+}
